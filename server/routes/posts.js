16fd4c815eb61930d90b348ad6d36ec19745d932
Irfan Ullah Munna <shantocuet659@gmail.com>
2021-06-04 16:05:23 +0600

    use redux

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,20 @@
 import express from 'express';
 
+import { getPosts, createPost, getPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
+
+
 const router = express.Router();
 
 // http://localhost:5000/posts
 
-router.get('/', (req,res) => {
-    res.send('This Works');
-});
+router.get('/', getPosts);
+router.post('/', createPost);
+router.get('/:id', getPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
+
+
 
 export default router;
 
